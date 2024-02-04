d86a53b8161e7ea4c7236a65069649df3d202fc5
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-02-04 17:24:58 -0400

    Name your effect 💞

--- src/Effect.tsx
+++ src/Effect.tsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export function Effect() {
-  return <section>Effect</section>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  useEffect(
+    function updateFullNameOnNamesChange() {
+      setFullName(firstName + " " + lastName);
+    },
+    [firstName, lastName]
+  );
+
+  return (
+    <form>
+      <input
+        placeholder="Your first name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        placeholder="Your last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <p>{fullName}</p>
+    </form>
+  );
 }
