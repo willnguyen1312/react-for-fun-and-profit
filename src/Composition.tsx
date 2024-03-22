149b2f4b7c16bc2699e81131848db148e7c845e8
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-22 10:32:18 -0300

    Enhance composition component 💞

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,30 @@
 export function Composition() {
-  return <section>Composition.tsx</section>;
+  const firstName = "John";
+  const lastName = "Doe";
+  const fullName = `${firstName} ${lastName}`;
+
+  return (
+    <section>
+      <h1>Composition</h1>
+
+      <NameDisplay firstName={firstName} lastName={lastName} />
+
+      <Text value={fullName} />
+    </section>
+  );
+}
+
+function NameDisplay({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}) {
+  const fullName = `${firstName} ${lastName}`;
+  return <p>{fullName}</p>;
+}
+
+function Text({ value }: { value: string }) {
+  return <p>{value}</p>;
 }
